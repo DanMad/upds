05d025d12c583dcf4f5c231c9896cb71e0d654c1
Daniel Maddison <dmaddison@icloud.com>
2020-04-16 21:50:10 +1000

    added staging scripts to package.json

--- src/js/up.js
+++ src/js/up.js
@@ -2,15 +2,55 @@
 
 // UP Module to reference in application
 const UP = (() => {
-  const foo = (str) => {
-    alert(str);
+  // Public
+  const accordion = {
+    toggle: (node) => {
+      if (node.classList.contains(`addClassName`)) {
+        node.classList.remove(`addClassName`);
+
+        // add anime call
+      } else {
+        node.classList.add(`addClassName`);
+
+        // add anime call
+      }
+    },
+  };
+
+  const ping = (str) => {
+    console.log(str);
   };
 
   return {
-    foo,
+    accordion,
+    ping,
   };
 })();
 
-(() => {
-  // IIFE for application
+const CMSApp = (() => {
+  const _namespace = `up`;
+
+  const _entryPoint = document.querySelector(`#page-content`);
+  const _contentNodes = _entryPoint.querySelectorAll(
+    `h1, h2, h3, h4, h5, h6, p, li, strong, em, ol, ul, blockquote`
+  );
+
+  const _addUPClassNames = (nodes) => {
+    for (let i = 0; i < nodes.length; i++) {
+      const node = nodes[i];
+      let tag = node.tagName.toLowerCase();
+
+      console.log(`.${_namespace}-${tag}`);
+    }
+  };
+
+  const run = () => {
+    _addUPClassNames(_contentNodes);
+  };
+
+  return {
+    run,
+  };
 })();
+
+alert(`firing`);
